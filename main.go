069cfe5a8efa95f566e20d5c0fdbdb5c981d24d2
binarycat <dogedoge61@gmail.com>
2020-07-07 14:24:20 -0400

    made .files hidden

--- main.go
+++ main.go
@@ -6,29 +6,49 @@ import (
 	"io/ioutil"
 	"path/filepath"
 	"fmt"
+	"flag"
 )
 
+// FileInfoOut is a struct with the strings to print.
+// This is stored first so spacing can be correctly calculated.
+type FileInfoOut struct {
+	Name, Size string
+}
+
+var ignoreDotfiles = true
+
 func main() {
-	var dir string
-	if len(os.Args) == 1 {
+	flag.Parse()
+	var dir string = flag.Arg(0)
+	if dir == "" {
 		dir = "."
-	} else {
-		dir = os.Args[1]
 	}
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		panic(err)
 	}
-	for _, f := range files {
-		fmt.Println(f.Name())
+	var outputArr = make([]FileInfoOut,len(files))
+	for i, f := range files {
+		if ignoreDotfiles && f.Name()[0] == '.' {
+			continue
+		}
+		var size int64
+		if f.IsDir() {
+			size = CalculateTotalSize(filepath.Join(dir,f.Name()))
+		} else {
+			size = f.Size()
+		}
+		fmtdSize := FormatFileSize(size)
+		outputArr[i] = FileInfoOut{f.Name(),fmtdSize}
 	}
-	fmt.Println(FormatFileSize(CalculateTotalSize(dir)))
+	// fmt.Println(FormatFileSize(CalculateTotalSize(dir)))
+	fmt.Println(outputArr)
 }
 
-func CalculateTotalSize(path string) int64 {
+func CalculateTotalSize(dirpath string) int64 {
 	var size int64 = 0 // size in bytes
 
-	filepath.Walk(path,func (_ string, info os.FileInfo, err error) error {
+	filepath.Walk(dirpath,func (path string, info os.FileInfo, err error) error {
 		if info.IsDir() {
 			return nil
 		}
