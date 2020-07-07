1a8bd748b498526332fdaea874149e22414420cb
binarycat <dogedoge61@gmail.com>
2020-07-07 15:58:20 -0400

    added -t flag to show total directory size

--- main.go
+++ main.go
@@ -17,10 +17,14 @@ type FileInfoOut struct {
 }
 
 var showHidden = false
-
+var extraPadding = ""
+var showTotal bool
 func init() {
 	flag.BoolVar(&showHidden, "a",false, "shows files starting with '.'")
+	flag.StringVar(&extraPadding, "pad","", "a string of text to use as extra padding between file names and sizes")
+	flag.BoolVar(&showTotal, "t", false, "show the total size of the directory")
 }
+
 
 func main() {
 	flag.Parse()
@@ -32,6 +36,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	var totalSize int64 // only used if showTotal == true
 	var outputArr = make([]FileInfoOut,len(files))
 	for i, f := range files {
 		if !showHidden && f.Name()[0] == '.' {
@@ -39,18 +44,26 @@ func main() {
 		}
 		var size int64
 		if f.IsDir() {
-			size = CalculateTotalSize(filepath.Join(dir,f.Name()))
+			size = CalculateSize(filepath.Join(dir,f.Name()))
 		} else {
 			size = f.Size()
+		}
+		if showTotal {
+			totalSize += size
 		}
 		fmtdSize := FormatFileSize(size)
 		outputArr[i] = FileInfoOut{f.Name(),fmtdSize}
 	}
+	if showTotal {
+		outputArr = append(outputArr, FileInfoOut{Name: "total:",Size: FormatFileSize(totalSize)})
+	}
+
+
 	// FmtOutput adds a newline, so we don't do it again
-	fmt.Print(FmtOutput(outputArr))
+	fmt.Print(FmtOutput(outputArr,FmtOutputOptions{ExtraPadding: extraPadding}))
 }
 
-func CalculateTotalSize(dirpath string) int64 {
+func CalculateSize(dirpath string) int64 {
 	var size int64 = 0 // size in bytes
 
 	filepath.Walk(dirpath,func (path string, info os.FileInfo, err error) error {
@@ -74,7 +87,11 @@ func FormatFileSize(size int64) string {
 	return fmt.Sprintf("%d%v",size,MetricBinarySuffixes[i])
 }
 
-func FmtOutput(fInfo []FileInfoOut) string {
+type FmtOutputOptions struct {
+	// ExtraPadding can be used to provide more distance between file names and sizes
+	ExtraPadding string
+}
+func FmtOutput(fInfo []FileInfoOut,opt FmtOutputOptions) string {
 	var maxNameLength int = 0
 
 	for _, inf := range fInfo {
@@ -84,13 +101,20 @@ func FmtOutput(fInfo []FileInfoOut) string {
 	}
 
 	bldr := strings.Builder{}
+
 	// we want to be able to store len(fInfo) lines.
 	// the length of a line is maxNameLength (all names will be padded up to this with spaces)
 	// + 1 for newLine
 	// + 7 for filesize (4 digits + 3 for suffix (unless suffix is B, but we dont check that)) and
 	// another + 1 for padding
-	bldr.Grow((maxNameLength + 1 + (4 + 3) + 1) * len(fInfo))
+	var bldrLen = (maxNameLength + 1 + (4 + 3) + 1) * len(fInfo)
+	if opt.ExtraPadding != "" {
+		// increase length for ExtraPadding, if it is used
+		bldrLen += len(opt.ExtraPadding) * len(fInfo)
+	}
+	bldr.Grow(bldrLen)
 	for _, inf := range fInfo {
+		// Filter out zero entries left by hidden files.
 		if (inf == FileInfoOut{}) {
 			continue
 		}
@@ -99,8 +123,10 @@ func FmtOutput(fInfo []FileInfoOut) string {
 			// Pad name with spaces to make all names the same width (so sizes align)
 			bldr.WriteRune(' ')
 		}
-		// One more space for extra padding
+		// One more space for padding
 		bldr.WriteRune(' ')
+		// Add ExtraPadding (no need for an if statement, it will be ignored if empty)
+		bldr.WriteString(opt.ExtraPadding)
 		bldr.WriteString(inf.Size)
 		bldr.WriteRune('\n')
 	}
