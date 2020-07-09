daefc90979e503899260d2e44eedf220e7bb4479
binarycat <dogedoge61@gmail.com>
2020-07-09 11:43:58 -0400

    moved some code to format.go

--- main.go
+++ main.go
@@ -8,18 +8,29 @@ import (
 	"flag"
 	"fmt"
 	"path/filepath"
-	"strings"
+
 
 	//"time"
 	"github.com/karrick/godirwalk"
 	dirwalk "github.com/karrick/godirwalk"
 )
 
-// FileInfoOut is a struct with the strings to print.
+// FileInfoOut is a struct with the info to print.
 // This is stored first so spacing can be correctly calculated.
 type FileInfoOut struct {
 	Name, Size string
 }
+type FileInfoOutList []FileInfoOut
+
+// FileInfo is the type used before Size is formatted into a string
+// Mostly used to be sorted
+type FileInfo struct {
+	Name string
+	Size int64
+}
+
+type FileInfoList []*FileInfo
+
 
 var scratchBuffer []byte
 
@@ -50,7 +61,7 @@ func main() {
 		panic(err)
 	}
 	var totalSize int64 // only used if showTotal == true
-	var outputArr = make([]FileInfoOut,len(files))
+	var outputArr = make(FileInfoList,len(files))
 	for i, f := range files {
 		if err != nil {
 			panic(err)
@@ -67,16 +78,17 @@ func main() {
 		if showTotal {
 			totalSize += size
 		}
-		fmtdSize := FormatFileSize(size)
-		outputArr[i] = FileInfoOut{f.Name(),fmtdSize}
+		//fmtdSize := FormatFileSize(size)
+		outputArr[i] = &FileInfo{f.Name(),size}
 	}
 	if showTotal {
-		outputArr = append(outputArr, FileInfoOut{Name: "total:",Size: FormatFileSize(totalSize)})
+		outputArr = append(outputArr, &FileInfo{Name: "total:",Size: totalSize})
 	}
 
 
 	// FmtOutput adds a newline, so we don't do it again
-	fmt.Print(FmtOutput(outputArr,FmtOutputOptions{ExtraPadding: extraPadding}))
+	fmt.Print(FmtOutput(FmtFileInfoList(outputArr),
+			FmtOutputOptions{ExtraPadding: extraPadding} ))
 }
 
 func CalculateSize(dirpath string) int64 {
@@ -115,57 +127,5 @@ func CalculateSize(dirpath string) int64 {
 
 var MetricBinarySuffixes = [...]string{"B","kiB","MiB","GiB"}
 
-func FormatFileSize(size int64) string {
-	i := 0
-	for size >= 1024 {
-		i++
-		size = size >> 10
-	}
-	return fmt.Sprintf("%d%v",size,MetricBinarySuffixes[i])
-}
 
-type FmtOutputOptions struct {
-	// ExtraPadding can be used to provide more distance between file names and sizes
-	ExtraPadding string
-}
-func FmtOutput(fInfo []FileInfoOut,opt FmtOutputOptions) string {
-	var maxNameLength int = 0
 
-	for _, inf := range fInfo {
-		if len(inf.Name) > maxNameLength {
-			maxNameLength = len(inf.Name)
-		}
-	}
-
-	bldr := strings.Builder{}
-
-	// we want to be able to store len(fInfo) lines.
-	// the length of a line is maxNameLength (all names will be padded up to this with spaces)
-	// + 1 for newLine
-	// + 7 for filesize (4 digits + 3 for suffix (unless suffix is B, but we dont check that)) and
-	// another + 1 for padding
-	var bldrLen = (maxNameLength + 1 + (4 + 3) + 1) * len(fInfo)
-	if opt.ExtraPadding != "" {
-		// increase length for ExtraPadding, if it is used
-		bldrLen += len(opt.ExtraPadding) * len(fInfo)
-	}
-	bldr.Grow(bldrLen)
-	for _, inf := range fInfo {
-		// Filter out zero entries left by hidden files.
-		if (inf == FileInfoOut{}) {
-			continue
-		}
-		bldr.WriteString(inf.Name)
-		for i := len(inf.Name); i < maxNameLength; i++ {
-			// Pad name with spaces to make all names the same width (so sizes align)
-			bldr.WriteRune(' ')
-		}
-		// One more space for padding
-		bldr.WriteRune(' ')
-		// Add ExtraPadding (no need for an if statement, it will be ignored if empty)
-		bldr.WriteString(opt.ExtraPadding)
-		bldr.WriteString(inf.Size)
-		bldr.WriteRune('\n')
-	}
-	return bldr.String()
-}
